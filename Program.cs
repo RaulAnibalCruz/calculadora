198110ff1a5eb53f9af93dfe2e7e2315304974f3
RaulAnibalCruz <raul.cruzet12d1@gmail.com>
2025-04-16 17:06:22 -0300

    CALCULADORAAA

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿//CALCULADORA 
+Console.WriteLine(" opciones ");
+Console.WriteLine(" escribe 1 para sumar ");
+Console.WriteLine(" escribe 2 para multiplicar ");
+Console.WriteLine(" opciones ");
+Console.WriteLine(" opciones ");
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+float Calcular(float n1 , float n2, int opcion)
+{
+   
+
+    if (opcion == 1)
+    {
+        return n1 + n2;
+
+    }
+    if (opcion == 2)
+    {
+        return n1 * n2;
+
+    }
+    if (opcion == 3)
+    {
+        return n1 / n2;
+
+    }
+
+
+
+    return n1;
+}
+
+
