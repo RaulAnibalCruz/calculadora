363fe64367e4da3d99ed9c646ebee1ae0ab7c51b
RaulAnibalCruz <130664462+RaulAnibalCruz@users.noreply.github.com>
2025-04-23 18:26:50 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,43 +1,79 @@
-﻿//CALCULADORA 
-Console.WriteLine(" opciones ");
-Console.WriteLine(" escribe 1 para sumar ");
-Console.WriteLine(" escribe 2 para multiplicar ");
-Console.WriteLine(" opciones ");
-Console.WriteLine(" opciones ");
-
-Console.ReadLine();
-
-
-
-
-
-
-
-
-
-float Calcular(float n1 , float n2, int opcion)
+using System;
+class Calculadora
 {
-   
-
-    if (opcion == 1)
+    static void Main ()
     {
-        return n1 + n2;
+        Console.WriteLine("Calculadora basica de c# usando switch");
 
+        Console.WriteLine("Ingrese un numero: ");
+        double numero1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("seleccione una operacion  (+,-,x o /) : ");
+        string operacionAsing = Console.ReadLine(); //el string se usa para almacenar el texto asignado arriba
+
+        double resultado;
+
+        string masOperaciones = "SI";
+
+        Calculadora calculadora = new Calculadora();
+
+       
+
+            switch (operacionAsing)
+            {
+                case "+":
+                    //resultado = numero1 + numero2;
+                    calculadora.suma();
+                    Console.WriteLine("resultado es :" + resultado); //El "+" sirve como una concatenación osea une texto y datos en string
+                    break; //define la sentecai de un bucle y un switch
+
+                case "-":
+                    //resultado = numero1 + numero1;
+                    Console.WriteLine("Resultado de la resta es :" + calculadora.Resta(numero1, numero2));
+                    break;
+
+                case "x":
+                    resultado = numero1 * numero2;
+                    Console.WriteLine("resultado es :" + resultado);
+                    break;
+
+                case "/":
+                    if (numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+                        Console.WriteLine("el resultado es:" + resultado);
+                    }
+                    else
+                    {
+                        Console.WriteLine("no se puede dividir entre 0");
+
+                    }
+                    break;
+
+                //default se realizara en switch sin ninguna las expeciones coincide con case en switch
+                default:
+                    Console.WriteLine("no se a introducido ningun valor, utiliza +,-,x o /");
+                    break;
+
+                while (masOperaciones == "SI")
+                {
+
+                }
+        }
+
+        
     }
-    if (opcion == 2)
-    {
-        return n1 * n2;
 
+    void Sumar() { 
+    
     }
-    if (opcion == 3)
-    {
-        return n1 / n2;
 
+    static double Resta(double a, double b) {
+        return a + b;
     }
 
 
-
-    return n1;
 }
-
-
